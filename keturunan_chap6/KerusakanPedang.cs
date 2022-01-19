217055aa1a37b8b59c52bedac322844ff8d99a74
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 16:32:29 +0800

    Add project files.

---
+++ keturunan_chap6/KerusakanPedang.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace keturunan_chap6
+{
+    class KerusakanPedang
+    {
+
+    }
+}
---
+++ keturunan_chap6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace keturunan_chap6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
