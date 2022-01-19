dbc530c18138ecae8d5aa48635552fa848b84b89
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-19 17:12:11 +0800

    second progresszz

--- keturunan_chap6/Program.cs
+++ keturunan_chap6/Program.cs
@@ -6,7 +6,32 @@ namespace keturunan_chap6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            KerusakanPanah kerusakan_panah = new KerusakanPanah(LemparDadu(1));
+            KerusakanPedang kerusakan_pedang = new KerusakanPedang(LemparDadu(3));
+
+
+        } //END main ----------------------------------------------------------------------
+
+        public void LemparDadu(int n)
+        {
+            
+            for (int i = 0; i < n; i++)
+            {
+                kped.Roll = ngacak.Next(1, 7) + ngacak.Next(1, 7) + ngacak.Next(1, 7);
+                kped.AturNyalaApi(flaming.IsChecked.Value);
+                kped.AturKeajaiban(magic.IsChecked.Value);
+            }
+
+            DisplayKerusakan();
+        } //END function LemparDadu----------------------------------------------------------
+        
+        //void DisplayKerusakan()
+        //{
+        //    //damage is variable from Textblock's XAML
+        //    //damage.Text = "Rolled " + kped.Roll + " for " + kped.Kerusakan + " HP";
+        //    damage.Text = "Rolled " + kped.Roll + " for " + kped.Kerusakan + " HP";
+        //}
+
+
+    }//end class
+}//end namespace
